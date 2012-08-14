041e569278bff40c1da6509eaf0e5de8454f9917
Phillip Gore <phillipagore@me.com>
2012-08-14 16:18:24 -0400

    payment update

--- app.rb
+++ app.rb
@@ -56,6 +56,25 @@ class TheKeynoteStore < Sinatra::Base
 		end
 	end
 
+	after '/payment' do
+		@serial = rand(1000000000000000..9999999999999999)
+		if @purchase_count >= 3
+			@order.update(:order_number => @serial, :order_discount => @discount_percentage, :order_total => @purchase_total.to_i)
+		else
+			@order.update(:order_number => @serial, :order_total => @purchase_total.to_i)
+		end
+		@purchase_hash.each do |key, value|
+			@theme = Theme.get(key.to_i)
+			@purchase = @order.purchases.create(
+				:item_name => @theme.name,
+				:item_id => @theme.id,
+				:item_quantity => value.to_i,
+				:item_price => @theme.price
+			)
+		end
+		session['purchase'] = nil
+		redirect "/order/#{@order.id}"
+	end
 
 	
 # Theme Site
