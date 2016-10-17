5a4d8b4dcbc073d760f997a097ea8f192c41ad65
Phillip Gore <phillipagore@me.com>
2016-10-17 11:30:44 -0500

    hotfix

--- app.rb
+++ app.rb
@@ -116,14 +116,11 @@ class TheKeynoteStore < Sinatra::Base
 					logger.info  "PHILLIP GORE - AWS access_key_id: #{ENV['ACCESS_KEY_ID']}"
 					logger.info  "PHILLIP GORE - AWS secret_access_key: #{ENV['SECRET_ACCESS_KEY']}"
 					logger.info  "PHILLIP GORE - AWS bucket: #{ENV['S3_BUCKET']}"
-#					AWS.config(
-#						:access_key_id => ENV['ACCESS_KEY_ID'],
-#						:secret_access_key => ENV['SECRET_ACCESS_KEY']
-#					)
-					@s3 = AWS::S3.new(
+					AWS.config(
 						:access_key_id => ENV['ACCESS_KEY_ID'],
 						:secret_access_key => ENV['SECRET_ACCESS_KEY']
 					)
+					@s3 = AWS::S3.new
 					@url = @s3.buckets[ENV['S3_BUCKET']].objects["#{@theme.name.downcase.gsub(" ", "-")}.zip"].url_for(:read, :expires => 86400)
 					@purchase = @order.purchases.create(
 						:item_name => @theme.name,
